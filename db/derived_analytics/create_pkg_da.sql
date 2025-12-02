ae0e1f920f15cff313011817688cfd61e2266c2e
peter-m-kuehn <info@peter-kuehn.de>
2025-12-02 16:26:33 +0100

    bug fixed

--- db/derived_analytics/create_pkg_da.sql
+++ db/derived_analytics/create_pkg_da.sql
@@ -9,20 +9,37 @@ END da;
 
 CREATE OR REPLACE PACKAGE BODY da as
 
+	function calc_win_percent(p_centipawns in double) return double
+	as
+		v_win_percent double;
+	begin
+		logging.log('calc_win_percent()');
+		logging.log('p_centipawns: '||p_centipawns);
+		v_win_percent := 50.0 + 50.0 * (2.0 / (1.0 + exp(-0.00368208 * p_centipawns)) - 1.0);
+		logging.log('v_win_percent: '||v_win_percent);
+		return v_win_percent;
+	end calc_win_percent;
+	
 	function calc_accuracy (p_winpercent_before in double, p_winpercent_after in double) return double
 	as
 		v_win_diff double;
 		v_accuracy double;
 	begin
+logging.log('calc_accuracy()');
+logging.log('p_winpercent_before: '||p_winpercent_before);
+logging.log('p_winpercent_after: '||p_winpercent_after);
+
 		if p_winpercent_after >= p_winpercent_before then
+		logging.log('accuracy: 100.0');
 			return (100.0);
 		end if;
 	
 		v_win_diff := p_winpercent_before - p_winpercent_after;
-		v_accuracy := 103.1668100711649 * exp(-0.04354415386753951 * v_win_diff) + -3.166924740191411;
-		v_accuracy := v_accuracy + 1.0; -- uncertainty bonus (due to imperfect analysis)
+logging.log('v_win_diff: '||v_win_diff);
+		v_accuracy := 103.1668 * exp(-0.04354 * (v_win_diff)) - 3.1669;
 		v_accuracy := greatest(v_accuracy, 0.0);
 		v_accuracy := least(v_accuracy, 100.0);
+logging.log('v_accuracy: '||v_accuracy);
 		
 		return(v_accuracy);
 	end calc_accuracy;
@@ -97,52 +114,69 @@ CREATE OR REPLACE PACKAGE BODY da as
 	    order by g.id, p.half_move_num
 		)
 		loop
+		logging.log('position_id: '||v_rec.position_id);
 			if v_rec.best_move_uci_prv is null or v_rec.game_id <> v_rec.game_id_prv
 			then
 				continue; -- no move evaluation available
 			end if;
 			v_da_position_rec := null;
 			v_total_prv := v_rec.wins_prv + v_rec.draws_prv + v_rec.losses_prv;
+		logging.log('v_total_prv: '||v_total_prv);
 			v_win_rate_prv := v_rec.wins_prv / v_total_prv;
+		logging.log('v_win_rate_prv: '||v_win_rate_prv);
 			v_loss_rate_prv := v_rec.losses_prv / v_total_prv;
+		logging.log('v_loss_rate_prv: '||v_loss_rate_prv);
 			
-			if ((v_rec.half_move_num - 1) MOD 2 = 1) 
-			THEN
-				v_white_winning_chances_prv := 100 * v_win_rate_prv;
-				v_black_winning_chances_prv := 100 * v_loss_rate_prv;
-			else
-				v_black_winning_chances_prv := 100 * v_win_rate_prv;
-				v_white_winning_chances_prv := 100 * v_loss_rate_prv;
+			if ((v_rec.half_move_num - 1) MOD 2 = 0) 
+			then -- prior black move
+			    logging.log('black moved');
+				v_black_winning_chances_prv := calc_win_percent(v_rec.centipawn_prv);
+				v_white_winning_chances_prv := 100 - v_black_winning_chances_prv;
+			else -- white moved
+				logging.log('white moved');
+				v_white_winning_chances_prv := calc_win_percent(v_rec.centipawn_prv);
+				v_black_winning_chances_prv := 100 - v_white_winning_chances_prv;
 			end if;		
-		
+		logging.log('v_white_winning_chances_prv: '||v_white_winning_chances_prv);
+		logging.log('v_black_winning_chances_prv: '||v_black_winning_chances_prv);
+	
 			v_total := v_rec.wins + v_rec.draws + v_rec.losses;
+		logging.log('v_total: '||v_total);
 			v_score := v_rec.wins + v_rec.draws / 2;
+		logging.log('v_score: '||v_score);
 			v_score_rate := v_score / v_total;
+		logging.log('v_score_rate: '||v_score_rate);
 			v_win_rate := v_rec.wins / v_total;
+		logging.log('v_win_rate: '||v_win_rate);
 			v_draw_rate := v_rec.draws / v_total;
+		logging.log('v_draw_rate: '||v_draw_rate);
 			v_loss_rate := v_rec.losses / v_total;
+		logging.log('v_loss_rate: '||v_loss_rate);
 			
-			if (v_rec.half_move_num MOD 2 = 1) 
-			then -- white
-				v_white_winning_chances := 100 * v_win_rate;
-				v_white_score_rate := 100 * v_score_rate;
-				if (v_rec.move_white = v_rec.best_move_uci_prv) then
-					v_accuracy := 100.0;
-				else
-				    v_accuracy := calc_accuracy(v_white_winning_chances_prv, v_white_winning_chances);
-				end if;
-			    v_black_winning_chances := 100 * v_loss_rate;
-				v_black_score_rate := 100 * (1 - v_score_rate);
-			else -- black
-				v_black_winning_chances := 100 * (v_win_rate);
+			v_white_winning_chances := calc_win_percent(v_rec.centipawn);
+			v_black_winning_chances := 100 - v_white_winning_chances;
+			logging.log('v_white_winning_chances: '||v_white_winning_chances);
+			logging.log('v_black_winning_chances: '||v_black_winning_chances);
+				
+			if (v_rec.half_move_num MOD 2 = 0) 
+			then -- black moved
+				logging.log('black moved');
 				v_black_score_rate := 100 * v_score_rate;
+				v_white_score_rate := 100 * (1 - v_score_rate);
 				if (v_rec.move_black = v_rec.best_move_uci_prv) then
 					v_accuracy := 100.0;
 				else
 				    v_accuracy := calc_accuracy(v_black_winning_chances_prv, v_black_winning_chances);
 				end if;
-				v_white_winning_chances := 100 * v_loss_rate;
-				v_white_score_rate := 100 * (1 - v_score_rate);
+			else -- white moved
+				logging.log('white moved');
+				v_white_score_rate := 100 * v_score_rate;
+				v_black_score_rate := 100 * (1 - v_score_rate);
+				if (v_rec.move_white = v_rec.best_move_uci_prv) then
+					v_accuracy := 100.0;
+				else
+				    v_accuracy := calc_accuracy(v_white_winning_chances_prv, v_white_winning_chances);
+				end if;
 			end if;
 			v_white_draw_rate := 100 * v_draw_rate;
 			v_black_draw_rate := 100 * v_draw_rate;
@@ -152,7 +186,6 @@ CREATE OR REPLACE PACKAGE BODY da as
 			--
 			v_da_position_rec.position_id := v_rec.position_id;
 			v_da_position_rec.white_winning_chances := v_white_winning_chances;
-			v_da_position_rec.white_score_rate := v_white_score_rate;
 			v_da_position_rec.white_score_rate := v_white_score_rate;
 			v_da_position_rec.white_draw_rate := v_white_draw_rate;
 			v_da_position_rec.black_winning_chances := v_black_winning_chances;
