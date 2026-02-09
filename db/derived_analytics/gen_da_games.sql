f23f9626902e75ba5a09f4c7e74d9463f86e70f8
peter-m-kuehn <info@peter-kuehn.de>
2026-02-09 12:26:06 +0100

    more scripts

---
+++ db/derived_analytics/gen_da_games.sql
@@ -0,0 +1,31 @@
+SET SESSION SQL_MODE='ORACLE';
+DELIMITER //
+begin
+	da.gen_da_game;
+	commit;
+end;
+//
+
+DELIMITER ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ db/derived_analytics/gen_da_positions.sql
@@ -0,0 +1,52 @@
+SET SESSION SQL_MODE='ORACLE';
+DELIMITER //
+begin
+	da.gen_da_position(4393582);
+	da.gen_da_position(4345768);
+	da.gen_da_position(4610064);
+	da.gen_da_position(4353656);
+	da.gen_da_position(4350300);
+	da.gen_da_position(4510255);
+	da.gen_da_position(4387471);
+	da.gen_da_position(4352896);
+	da.gen_da_position(4388401);
+	da.gen_da_position(4390927);
+	da.gen_da_position(4374091);
+	da.gen_da_position(4352339);
+	da.gen_da_position(4345769);
+	da.gen_da_position(4364570);
+	da.gen_da_position(4395089);
+	da.gen_da_position(4375094);
+	da.gen_da_position(4377282);
+	da.gen_da_position(4376890);
+	da.gen_da_position(4348903);
+	da.gen_da_position(4373738);
+	da.gen_da_position(4375261);
+	da.gen_da_position(4419751);
+	commit;
+end;
+//
+
+DELIMITER ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
